679c8bf12313aa76b5f6f0b501e4ea8491a0c579
domasgr <44139891+domasgr@users.noreply.github.com>
2018-11-21 11:21:41 +0200

    added mysql connect

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,5 +1,6 @@
 <?php
-$db = new mysqli('localhost', 'root', 'root', 'projects');
+//mysql://be27f95009fb7d:64b660d2@us-cdbr-iron-east-01.cleardb.net/heroku_33338926e8b69c3?reconnect=true
+$db = new mysqli('us-cdbr-iron-east-01.cleardb.net', 'be27f95009fb7d', '64b660d2', 'heroku_33338926e8b69c3');
 // print_r($db);
 if($db->connect_errno != 0){
     die("Error in DB connection $db->connect_error");
